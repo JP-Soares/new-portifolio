79b850bbca10500665325556a42d47b6b2d8e081
JP <pedrojoao_costa@hotmail.com>
2026-04-07 16:27:27 +0100

    creating the files

---
+++ src/components/menu.tsx
@@ -0,0 +1,11 @@
+export default function MenuBar(){
+    return(
+        <nav>
+            <li>
+                <ul>About</ul>
+                <ul>Projects</ul>
+                <ul>Contact</ul>
+            </li>
+        </nav>
+    );
+}
