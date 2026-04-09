4d025f183cefe04f53df0b50470ce4993223b962
JP <pedrojoao_costa@hotmail.com>
2026-04-09 11:00:51 +0100

    styling about and header

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -4,10 +4,10 @@ export default function Header(){
     return(
         <div>
             <div className="p-40">
-                <p className="text-2xl ">João Pedro</p>
-                <p>Full-Stack Developer focused on backend systems and real-world solutions.</p>
+                <p className="text-2xl text-jacksons-purple-800">João Pedro</p>
+                <p className="text-jacksons-purple-800">Full-Stack Developer focused on backend systems and real-world solutions.</p>
 
-                <p className="pt-5">I build scalable APIs, work with databases, and create systems that 
+                <p className="pt-5 text-jacksons-purple-800">I build scalable APIs, work with databases, and create systems that 
                     solve business problems efficiently.
                 </p>
             </div>
