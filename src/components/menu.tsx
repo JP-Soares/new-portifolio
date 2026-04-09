7ac32cb196f4a0a9ae17aa3fb6d8a4c5d1f377a4
JP <pedrojoao_costa@hotmail.com>
2026-04-09 10:42:45 +0100

    adding color palete

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -1,7 +1,7 @@
 export default function MenuBar(){
     return(
-        <nav className="flex justify-center items-center h-16 bg-gray-100">
-            <ul className="flex space-x-10">
+        <nav className="flex justify-center items-center h-16 bg-jacksons-purple-100">
+            <ul className="flex space-x-10 text-jacksons-purple-800">
                 <li><a href="#">About</a></li>
                 <li><a href="#">Projects</a></li>
                 <li><a href="#">Tech Stack</a></li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // 👈 REQUIRED
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'jacksons-purple': {
+          50: 'var(--color-jacksons-purple-50)',
+          100: 'var(--color-jacksons-purple-100)',
+          200: 'var(--color-jacksons-purple-200)',
+          300: 'var(--color-jacksons-purple-300)',
+          400: 'var(--color-jacksons-purple-400)',
+          500: 'var(--color-jacksons-purple-500)',
+          600: 'var(--color-jacksons-purple-600)',
+          700: 'var(--color-jacksons-purple-700)',
+          800: 'var(--color-jacksons-purple-800)',
+          900: 'var(--color-jacksons-purple-900)',
+          950: 'var(--color-jacksons-purple-950)',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
